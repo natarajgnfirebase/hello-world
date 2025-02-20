855f513b21d4c071b0a3353a7a9f59612bda2e17
natarajgnfirebase <gn.natarajfirebase@gmail.com>
2025-02-20 16:57:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World!!!')
+import argparse
+
+parser = argparse.ArgumentParser(description="Process extra vars")
+parser.add_argument("--name", type=str, help="User's name")
+parser.add_argument("--age", type=int, help="User's age")
+parser.add_argument("--location", type=str, help="User's location")
+
+args = parser.parse_args()
+
+print("Received parameters:")
+print(f"Name: {args.name}")
+print(f"Age: {args.age}")
+print(f"Location: {args.location}")
